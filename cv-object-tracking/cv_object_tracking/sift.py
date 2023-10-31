91db2e93c52633e46318c2468a42d9135817162e
Isha Goyal <goyalisha03@gmail.com>
2023-10-31 14:34:32 -0400

    Loops through photos and does keypoint matching for each frame.

--- cv-object-tracking/cv_object_tracking/sift.py
+++ cv-object-tracking/cv_object_tracking/sift.py
@@ -1,39 +1,75 @@
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
+import os
+from os import listdir
 
-img1 = cv.imread('cv-object-tracking/images_and_videos/medical_professionals/1/images/first_frame_00.png', cv.IMREAD_GRAYSCALE)
-img2 = cv.imread('cv-object-tracking/images_and_videos/medical_professionals/1/images/first_frame_01.png', cv.IMREAD_GRAYSCALE)
+folder_dir = "cv-object-tracking/images_and_videos/medical_professionals/1/images/"
+numImages = len(os.listdir(folder_dir))
+
+# create array with all the file names. separate cases for 0-9 and >9 so that they always have two digits
+# assumption: you have more than 9 images
+imNames = [f"first_frame_0{i}.png" for i in range(0, 10)] + [f"first_frame_{i}.png" for i in range(10, numImages)]
 
 # Initiate SIFT detector
 sift = cv.SIFT_create()
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
 
-# BFMatcher with default params
-bf = cv.BFMatcher()
-matches = bf.knnMatch(des1,des2,k=2)
+# loop through and play images
+for i in range(numImages - 1):
+    img1 = cv.imread(folder_dir + imNames[i], cv.IMREAD_GRAYSCALE)
+    img2 = cv.imread(folder_dir + imNames[i+1], cv.IMREAD_GRAYSCALE)
 
-# Apply ratio test
-good = []
-for m,n in matches:
-    if m.distance < 0.75*n.distance:
-        good.append([m])
-# cv.drawMatchesKnn expects list of lists as matches.
-img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
 
-plt.imshow(img3),plt.show()
+    # BFMatcher with default params
+    bf = cv.BFMatcher()
+    matches = bf.knnMatch(des1,des2,k=2)
 
-# kp = sift.detect(gray,None)
-# img=cv.drawKeypoints(gray,kp,img)
-# cv.imwrite('first_frame_00_sift_keypoints.png',img)
+    # # Apply ratio test
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append([m])
 
-# img=cv.drawKeypoints(gray,kp,img,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-# cv.imwrite('first_frame_00_sift_keypoints.png',img)
+    # cv.drawMatchesKnn expects list of lists as matches.
+    img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-# img = cv.imread('first_frame_00_sift_keypoints.png')
-# cv.imshow('image', img)
+    plt.imshow(img3),plt.show()
+    
+    # cv.imshow('image', img1)
+    # cv.waitKey(40)
+
+
+# next steps:
+# - use ROI to pick the object after the first frame
+# - only show the keypoints that fall within that region
+# - show the progression of images with only those keypoints
+# - maybe, find centroid
+# - maybe, recalculate ROI in every frame
+
+# cv.imshow('image', img1)
 # cv.waitKey(0)
 
-# des = cv.compute(gray, kp)
+# img2 = cv.imread('cv-object-tracking/images_and_videos/medical_professionals/1/images/first_frame_01.png', cv.IMREAD_GRAYSCALE)
+
+
+# find the keypoints and descriptors with SIFT
+# kp1, des1 = sift.detectAndCompute(img1,None)
+# kp2, des2 = sift.detectAndCompute(img2,None)
+
+# BFMatcher with default params
+# bf = cv.BFMatcher()
+# matches = bf.knnMatch(des1,des2,k=2)
+
+# # Apply ratio test
+# good = []
+# for m,n in matches:
+#     if m.distance < 0.75*n.distance:
+#         good.append([m])
+
+# cv.drawMatchesKnn expects list of lists as matches.
+# img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+# plt.imshow(img3),plt.show()
